d684e837348a8299f4afab5219f89b833909fca0
NkMasy <kreizosorhey@gmail.com>
2025-02-25 18:22:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'KreizoS', // Your name
+  botOwner: 'KreizS', // Your name
   ownerNumber: '917638942753', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
